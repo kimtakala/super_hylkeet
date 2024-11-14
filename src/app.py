9e5fe7fc0431d6cf7d00477830675223c5f3778c
jasminlei <jasminleinonen@hotmail.com>
2024-11-14 13:52:31 +0200

    add route for adding refs to database

--- src/app.py
+++ src/app.py
@@ -1,39 +1,25 @@
-from flask import redirect, render_template, request, jsonify, flash
+from flask import redirect, render_template, jsonify
 from db_helper import reset_db
-from repositories.todo_repository import get_todos, create_todo, set_done
 from config import app, test_env
-from util import validate_todo
+from references import add_reference
+
 
 @app.route("/")
 def index():
-    todos = get_todos()
-    unfinished = len([todo for todo in todos if not todo.done])
-    return render_template("index.html", todos=todos, unfinished=unfinished) 
+    return render_template("index.html")
 
-@app.route("/new_todo")
-def new():
-    return render_template("new_todo.html")
 
-@app.route("/create_todo", methods=["POST"])
-def todo_creation():
-    content = request.form.get("content")
-
-    try:
-        validate_todo(content)
-        create_todo(content)
-        return redirect("/")
-    except Exception as error:
-        flash(str(error))
-        return  redirect("/new_todo")
-
-@app.route("/toggle_todo/<todo_id>", methods=["POST"])
-def toggle_todo(todo_id):
-    set_done(todo_id)
+@app.route("/add_reference", methods=["POST"])
+def add_reference_route():
+    # validaatiofunktio tähän
+    add_reference()
     return redirect("/")
+
 
 # testausta varten oleva reitti
 if test_env:
+
     @app.route("/reset_db")
     def reset_database():
         reset_db()
-        return jsonify({ 'message': "db reset" })
+        return jsonify({"message": "db reset"})
