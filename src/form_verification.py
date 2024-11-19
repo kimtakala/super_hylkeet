aaaa8a67f4335af94a625ecb19380818d8a13bab
aatls <aatos.latva-somppi@helsinki.fi>
2024-11-19 22:54:50 +0200

    verify pages

--- src/form_verification.py
+++ src/form_verification.py
@@ -56,8 +56,11 @@ def verify_volume(volume):
 
 
 def verify_pages(pages):
+    if len(pages) < 2:
+        return "Pages field must include more than 1 character"
+
     if len(pages) > 50:
-        return "Pages field must include less than 50 characters"
+        return "Pages field must include less than 200 characters"
 
 
 def verify_publisher(publisher):
