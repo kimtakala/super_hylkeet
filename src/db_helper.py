57389f23f684dfbb79666b616440bd069ef4719d
Kim Takala <kim.takala@helsinki.fi>
2024-12-02 13:29:56 +0200

    corrected spelling

--- src/db_helper.py
+++ src/db_helper.py
@@ -33,7 +33,7 @@ TABLE_CITATIONS = """
                     timestamp TEXT
                 """
 
-TABLE_AUTHROS = """
+TABLE_AUTHORS = """
                     id SERIAL PRIMARY KEY,
                     citation_id INT NOT NULL,
                     first_name TEXT,
@@ -66,7 +66,7 @@ def reset_table(table_name):
 
 
 def setup_table(table_name):
-    tables = {"citations": TABLE_CITATIONS, "authors": TABLE_AUTHROS}
+    tables = {"citations": TABLE_CITATIONS, "authors": TABLE_AUTHORS}
 
     if table_name not in tables:
         raise ValueError(f"Table name {table_name} not regognized.")
